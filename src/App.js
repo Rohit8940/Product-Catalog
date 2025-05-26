af7b630981d1fb2e4b5f9614e08c93d17056fb5c
Rohit <baruahrohit222@gmail.com>
2025-05-26 18:47:18 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import './App.css';
